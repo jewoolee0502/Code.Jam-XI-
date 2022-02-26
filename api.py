b9d1001eec12149e67020d654ae329c29913ddf0
Jewoo Lee <jwcairo0502@gmail.com>
2022-02-26 16:46:04 -0500

    updated generate security key function

--- api.py
+++ api.py
@@ -29,37 +29,35 @@ class Predictions(Resource):
   
   def post(self):
       parser = reqparse.RequestParser() # initialize
-      parser.add_argument('screens') # add args
-      parser.add_argument('schedule')
+      parser.add_argument('screens', required=True) # add args
+      parser.add_argument('schedule', required=True)
         
       args = parser.parse_args() # parse arguments to dict
         
+        
+      accessKey = flask.request.args.get('access_token')
+        
+      if accessKey != key:
+            return self.unauthorized()
+      
       return {
-          'Prediction': '09090'
+          'Prediction': self.getImpressions()
       }, 200
         
-<<<<<<< HEAD
-        accessKey = flask.request.args.get('access_token')
         
-        if accessKey == key:
-              return self.getImpressions()
-        else:
-              return self.unauthorized()
+      
         
        
-    def requestKey(self, length):
-           return key
+  def requestKey(self, length):
+        return key
     
-    def unauthorized(self):
-          return 'UNAUTHORIZED', 401, {'content-type': 'application/json'}
+  def unauthorized(self):
+        return 'UNAUTHORIZED', 401, {'content-type': 'application/json'}
         
-    def getImpressions(self): #Claris function
-          return 0
+  def getImpressions(self): #Claris function
+        return 0
 
         
-
-=======
->>>>>>> 13b9377d26fabd340b2e24767c999eb41069c5fc
 api.add_resource(Predictions, '/predictions')
 
 if __name__ == "__main__":
